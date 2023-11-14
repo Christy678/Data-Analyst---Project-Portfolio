221c3d40e9de5b5aa3228bb381b85a9aa24d64e4
Christy678 <126855541+Christy678@users.noreply.github.com>
2023-11-13 22:12:57 -0800

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,81 @@
+# Calculate the multiplication and sum of two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+product = num1 * num2
+
+if product <= 1000:
+    print("Product is greater than or equal to 1000:", product)
+else:
+    print("Sum of the two numbers:", num1 + num2)
+    
+
+# Remove first 'n' characters from a string 
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a string")
+word = input("Enter your word: ")
+print(remove_chars(word, 4))
+print(remove_chars(word, 2))
+
+# Friend activity timers
+friends = ["Rolf", "Jane", "Anne", "Sarah", "Carolene"]
+Last_seen = ["3:00", "12:00", "7:00", "5:00", "6:00"]
+
+# Combine the two lists into a dictionary using zip
+timers = dict(zip(friends, Last_seen))
+
+print(timers)
+
+def multiply():
+    num1, num2 = map(int, input("Enter 2 numbers: ").split(','))
+    results = num1 * num2
+    return results
+
+print(multiply())
+
+# Print out numbers from 1 to 100 (including 100).
+# But, instead of printing multiples of 3, print "Fizz"
+# Instead of printing multiples of 5, print "Buzz"
+# Instead of printing multiples of both 3 and 5, print "FizzBuzz".
+
+for numbers in range(1, 101):
+    if numbers % 3 == 0:
+        print("Fizz")
+    elif numbers % 5 == 0:
+        print("Buzz")
+    elif numbers % 3 == 0 and numbers % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(numbers)
+        
+
+# random magic ball predictions 
+import random
+
+question = input("Ask me any question you like: ")
+random_number = random.randint(1, 9)
+
+
+answers_dict = {
+    1: "Yes - definitely",
+    2: "As the moon waxes, so shall your fortunes grow.",
+    3: "Without a doubt",
+    4: "The magic ball is on vacation from answering silly questions. Come back later!",
+    5: "The stars are too busy laughing at your question to provide a serious answer.",
+    6: "Is the sky blue? Ask again when pigs fly!",
+    7: "My sources say no",
+    8: "Even Cupid is scratching his head at your question. Life is mysterious, after all!",
+    9: "Very doubtful",
+}
+
+# Check if random_number is within the range of the dictionary keys
+if 1 <= random_number <= 9:
+    answer = answers_dict[random_number]
+else:
+    answer = "Error"
+
+print(answer)
